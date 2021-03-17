12ffff60308a012745aad3f82d0018f4bda14599
Jemar Mendones <jemar.mendones@gmail.com>
2021-03-17 16:55:56 +0800

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This sentence is mine!'));
+app.get('/', (req, res) => res.send('This sentence is STILL mine!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
